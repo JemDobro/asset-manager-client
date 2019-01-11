b134e7e53cb8c6ed3c684b8e3ff98c58608da4eb
Jemadean Dobrosielski <jemadean@gmail.com>
2019-01-10 18:57:41 -0800

    shortened msgs

--- src/validators.js
+++ src/validators.js
@@ -1,13 +1,16 @@
-export const required = value => value ? undefined : 'Please enter a value. This is a required field. Thanks!';
-export const nonEmpty = value => value.trim() !== '' ? undefined : 'Please enter a value. This field cannot be empty. Thanks!';
-export const isTrimmed = value => value.trim() === value ? undefined : 'Please check your entry. This field cannot start or end with whitespace. Thanks!';
+export const required = value => value ? undefined : 'Required field. Please enter a value.';
+export const nonEmpty = value => value.trim() !== '' ? undefined : 'Cannot be empty. Please enter a value.';
+export const isTrimmed = value => value.trim() === value ? undefined : 'Cannot start or end with whitespace.';
 export const length = length => value => {
   if (length.min && value.length < length.min) {
-    return `Please change your entry. This field must be at least ${length.min} characters long. Thanks!`;
+    return `
+    Must be at least ${length.min} characters long.`;
   }
   if (length.max && value.length > length.max) {
-    return `Please change your entry. This field must be at most ${length.max} characters long. Thanks!`;
+    return `Must be at most ${length.max} characters long.`;
   }
 };
 export const matches = field => (value, allValues) =>
-  field in allValues && value === allValues[field] ? undefined : 'Please change your entry. This field does not match your above entered password.  Thanks!';
+  field in allValues && value.trim() === allValues[field].trim()
+    ? undefined
+    : 'Does not match. Please adjust.';
